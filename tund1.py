4a9dd4ff3d76632f81f387be3296e055f683707d
Ksenia Plesakova <Ksenia.Plesakova@tthkee.onmicrosoft.com>
2025-02-25 15:59:43 +0200

    Tund1

---
+++ tund1.py
@@ -0,0 +1,30 @@
+from random import *
+from math import *
+from typing import Type
+# #Ülesanne 1
+# nimi=input("Mis on sinu nimi?: ")
+# vanus=int(input("Kui vana sa oled?: "))
+
+# print(f"Tere, maailm Tervitan sind {nimi} Sa oled {vanus} aastat vana.")
+# print("Tere, maailm! Tervitan sind",nimi,"Sa oled",vanus,"aastat vana.")
+# print("Tere, maailm! Tervitan sind"+nimi+"\nSa oled"+str(vanus)+"aastat vana.")
+# #Ülesanne 2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(f"Muutuja {vanus} on {type(vanus)} tüübi")
+# print(f"Muutuja {eesnimi} on {type(eesnimi)} tüübi")
+# print(f"Muutuja {pikkus} on {type(pikkus)} tüübi")
+# print(f"Muutuja {kas_käib_koolis} on {type(kas_käib_koolis)} tüübi")
+#Ülesanne 3
+# kommidearv=randint(1,100)
+# print(f"Laual on {kommidearv} kommid")
+# kommidvõtmud=int(input("Mitu kommi tahad ära võtta?"))
+# onjäänud=kommidearv-kommidvõtmud
+# print(f"Laual on jäänud {onjäänud} komme")
+#Ülesanne 4
+puuradius = 10
+
+puuradius=input("Mis on puu radius?: ")
+print(f"Puu radius on {puuradius} cm")
